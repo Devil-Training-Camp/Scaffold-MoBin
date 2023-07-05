d676841f338298e7edc836d5e605bf2043659c97
mobin <13594848919@163.com>
2023-07-05 20:36:52 +0800

    update: 修改脚手架风格为cmd规范

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,45 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk')
 const { program } = require('commander')
+const createProject = require('../lib/create')
 
-// name 是配置脚手架名称
-// usage 是配置命令格式
-// 取出package.json的版本号
+// 设置首行提示
+program.name('training').usage('<command> [option]')
+
+// 获取版本号
+program.version(`training-cli ${require('../package.json').version}`)
+
+// 配置create命令
 program
-  .name('training-cli')
-  .usage('<command> [option]')
-  .version(`training-cli ${require('../package.json').version}`)
+  .command('create <project-name>')
+  .description('create a new project')
+  .option('-f, --force', 'overwrite target directory if it exists')
+  .action((projectName, cmd) => {
+    createProject(projectName, cmd)
+  })
 
-program.parse()
+// 配置config命令
+program
+  .command('config [value]')
+  .description('inspect and modify the config')
+  .option('-g, --get <key>', 'get value by key')
+  .option('-s, --set <key> <value>', 'set option[key] is value')
+  .option('-d, --delete <key>', 'delete option by key')
+  .action((value, keys) => {
+    console.log(value, keys)
+  })
+
+// on监听help指令
+program.on('--help', function () {
+  console.log()
+  console.log(
+    `Run ${chalk.cyan(
+      'training-cli <command> --help'
+    )} for detailed usage of given command.`
+  )
+  console.log()
+})
+
+// 解析用户输入的参数
+program.parse(process.argv)
